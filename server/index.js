053154c74ae8fc1dbbc081a531a2b409a657c223
Anthony Munene <munene.anthony@gmail.com>
2014-03-10 12:01:29 +0000

    fix errors in order to pass tests

--- server/index.js
+++ server/index.js
@@ -1,44 +1,9 @@
 var express = require('express');
-var	routes = require('./routes');
-var	tasks = require('./routes/tasks');
-var	hbs = require('express-hbs');
-var	http = require('http');
-var	path = require('path');
-var	mongoskin = require('mongoskin');
-
+var mongoskin = require('mongoskin');
 
 //make the database connection
-var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {safe: true});
+var db = mongoskin.db('mongodb://localhost:27017/rest-api-test', {safe: true});
 var app = express();
-/*
- * we export the database object to all middlewares.
- * by doing so we will be able to perform database operations in the routes module
-*/
-app.use(function (req, res, next) {
-	req.db = {};
-	req.db.tasks = db.collection('tasks');
-	next();
-});
-app.locals.appname = 'Express.js Todo App';
-// Set global __basedir to the directory of the application entry-point file
-global.__basedir = path.resolve(__dirname, '../');
-
-// app.engine('html', consolidate.handlebars);
-// app.set('views', __basedir +'/views');
-// app.set('view engine', 'html');
-
-app.engine('hbs', hbs.express3({
-	partialsDir: __basedir + '/views/partials',
-	defaultLayout: __basedir + '/views/layout/default.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', __basedir + '/views');
-
-
-
-//setup static folder with middleware
-
-app.use(express.favicon());
 
 //print requests in the terminal window
 app.use(express.logger('dev'));
@@ -47,61 +12,78 @@ app.use(express.logger('dev'));
 app.use(express.urlencoded()); // Replace for depricated connect.bodyParser()
 app.use(express.json()); // Replace for depricated connect.bodyParser()
 
-//middleware workaround for HTTP methods that involves headers
-app.use(express.methodOverride());
 
-app.use(express.cookieParser());
-app.use(express.session({
-	secret: '59B93087-78BC-4EB9-99A-A61FC844F6C9'
-}));
-app.use(express.csrf());
 
-app.use(express.static(__basedir + '/public'));
-//expose CRSF to templates
-app.use(function (req, res, next) {
-	res.locals.csrf = req.csrfToken();
-	console.info(res.locals.csrf);
+app.param("collectionName", function (req, res, next, collectionName) {
+	req.collection = db.collection(collectionName);
 	return next();
 });
 
-
-//enable router plugin
-app.use(app.router);
-
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//execute this whenever a request matches route/RegExp with :task_id in it
-app.param('task_id', function (req, res, next, taskId) {
-	req.db.tasks.findById(taskId, function (error, task) {
-		if (error) return next(error);
-		if(!task) return next(new Error('Task is not found'));
-
-		//if found, store in the request and proceed to next middleware
-		req.task = task;
-		return next();
+app.get("/", function (req, res) {
+	res.send("Please select a collection, e.g collections/messages");
+});
+app.get("/collections/:collectionName", function (req, res, next) {
+	req.collection.find({}, {
+		limit: 10,
+		sort: [["_id", -1]]
+	}).toArray(function (e, results) {
+		if (e) {
+			return next(e);
+		}
+		console.log("results are", results);
+		res.send(results);
 	});
+});
+app.post("/collections/:collectionName", function (req, res, next) {
+	req.collection.insert(req.body, {},
+		function (e, results) {
+			if (e) {
+				return next(e);
+			}
+			res.send(results);
+		}
 
+	);
+});
+app.get("/collections/:collectionName/:id", function (req, res, next) {
+	console.log("using get...id equals ");
+	req.collection.findById(
+		req.params.id,
+		function (e, result) {
+			if (e) {
+				return next(e);
+			}
+			res.send(result);
+		}
+
+	);
+});
+app.put("/collections/:collectionName/:id", function (req, res, next) {
+	req.collection.updateById(
+		req.params.id,
+		{ $set: req.body }, {safe: true, multi: false},
+		function (e, result) {
+			if (e) {
+				return next(e);
+			}
+			res.send((result === 1) ? {msg: "success"} : {msg: "error"});
+		}
+
+	);
+});
+app.del("/collections/:collectionName/:id", function (req, res, next) {
+	req.collection.removeById(
+		req.params.id,
+		function (e, results) {
+			if (e) {
+				return next(e);
+			}
+			res.send((results === 1) ? {msg: "success"} : {msg: "error"});
+		}
+
+	);
 });
 
-//define our routes
 
-app.get('/', routes.index);
-app.get('/tasks', tasks.list);
-app.post('/tasks', tasks.markAllCompleted);
-app.post('/tasks', tasks.add);
-app.post('/tasks/:task_id', tasks.markCompleted);
-app.del('/tasks/:task_id', tasks.del);
-app.get('tasks/completed', tasks.completed);
-//handle mistyped urls
 
-app.all('*', function (req, res) {
-  res.send(404);
-});
-/*http.createServer(app).listen(3000, function(){
-  console.log('Express server listening on port ' + 3000);
-});*/
 module.exports = app;
